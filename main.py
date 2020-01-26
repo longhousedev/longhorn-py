cdc55a80855072825ef7a54a4486c764d4b58303
George <george.xp7100@gmail.com>
2020-01-26 23:16:37 +0100

    Added voice connect / disconnect functionality

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import discord
+from discord.ext import commands
 from configparser import ConfigParser
+
 
 config = ConfigParser()
 config.read('config.ini')
+
+bot = commands.Bot(command_prefix="!")
+
 token = config.get('auth', 'token')
 
-client = discord.Client()
-
-@client.event
+@bot.event
 async def on_ready():
     print("We have logged in as {0.user}")
 
-client.run(token)
+@bot.command()
+async def join(ctx):
+    global voice
+    voice = await ctx.message.author.voice.channel.connect()
+    #bot.connect(test)
+
+@bot.command()
+async def leave(ctx):
+    await voice.disconnect()
+
+bot.run(token)
+
