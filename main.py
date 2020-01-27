baacc27d9466b8b716b4500f8e1afc34c9c4ee7b
George <george.xp7100@gmail.com>
2020-01-27 17:07:16 +0100

    pytube music support (WIP)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+import pytube
 from discord.ext import commands
 from configparser import ConfigParser
 
@@ -18,6 +19,23 @@ async def on_ready():
 async def join(ctx):
     await ctx.message.author.voice.channel.connect()
     #bot.connect(test)
+
+@bot.command()
+async def play(ctx, arg):
+    print(arg)
+    query = pytube.YouTube(arg)
+    source = query.streams.filter(audio_codec='opus', only_audio=True).order_by('abr').desc().first()
+    track = source.stream_to_buffer()
+    title = source.title
+    print("Track downloaded!")
+    audio = discord.FFmpegOpusAudio(track, bitrate=160)
+    print("Audio encoded!")
+    msg = "Now playing: " + title
+    for voice in bot.voice_clients:
+        if voice.channel == ctx.message.author.voice.channel:
+            voice.play(audio)
+            voice.volume = 100
+    #await ctx.send(msg)
 
 @bot.command()
 async def leave(ctx):
